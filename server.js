64bafd9221cc8ee99f27da66950eb49415f21aaa
Shanon Athooya Saga <51292673+shanon3@users.noreply.github.com>
2019-10-29 10:49:33 +0700

    membuat request post

--- server.js
+++ server.js
@@ -5,5 +5,15 @@ const bodyParser = require("body-parser");
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+//membuat request post
+app.post('/hello', function(req, res){
+    const respon = {
+        statusCode: 200,
+        error: "",
+        message: "Hello json"
+    }
+    res.json(respon);
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
 
